771399147083c0d4976993276f5db5c5cb46d932
Bram Roos <bramroos@Brams-MacBook-Pro.local>
2020-10-23 12:49:05 +0200

    Google analytics

--- wp-content/themes/fullstory/header.php
+++ wp-content/themes/fullstory/header.php
@@ -30,15 +30,21 @@
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<?php wp_head(); ?>			
+		<?php wp_head(); ?>	
 		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-24952873-10"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-24952873-10');
+		</script>
+
 	</head>
 			
 	<body <?php body_class(); ?> data-barba="wrapper">
-
-		<!-- <div class="blob-container">
-			<div class="blob-inner"></div>
-		</div> -->
 
 		<div class="off-canvas-wrapper">
 			
